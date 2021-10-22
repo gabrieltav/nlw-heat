375e7dd90cfaeaaed8e89233542aaa0b16be0cf8
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-10-22 00:25:49 -0400

    feat: Login Box authenticate

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,13 +1,52 @@
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
 
+type AuthResponse = {
+    token: string;
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+}
+
 export function LoginBox() {
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=e3bad9f06b08a7896917`;
+
+    async function singIn(githubCode: string) {
+        const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        })
+
+        const { token, user } = response.data
+
+        localStorage.setItem('@dowhile:token', token)
+
+        console.log(user);
+    }
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes('?code=');
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code');
+
+            window.history.pushState({}, '', urlWithoutCode);
+
+            singIn(githubCode)
+        }
+    }, [])
+
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
-                <VscGithubInverted size={24} />
+            <a href={signInUrl} className={styles.signInWithGithub}>
+                <VscGithubInverted size="24" />
                 Entrar com o Github
             </a>
         </div>
